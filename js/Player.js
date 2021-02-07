b710ea5877178c5fab0bc4a977a6ce06b45dd284
AndrewSimons3 <simdrew07@gmail.com>
2021-02-06 20:14:23 -0600

    createTokens method complete

--- js/Player.js
+++ js/Player.js
@@ -4,10 +4,23 @@ class Player {
     this.id = id;
     this.color = color;
     this.active = false;
-    this.token = this.createTokens;
+    this.token = this.createTokens(21);
   }
 
-  createTokens() {
+  /**
+ * Creates token objects for player
+ * @param     {number}    num - Number of token objects to be created
+ * @returns   {Array}     An array of the newly created token objects
+ */
+  
+  createTokens(num) {
+    let tokens = [];
 
+    for (let i = 0; i < num, i++;) {
+      let token = new Token(i, this);
+      tokens.push(token)
+    }
+
+    return tokens;
   }
 }
