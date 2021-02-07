b0125f14f5fe6dcd4b69003ddb15a185336df4df
AndrewSimons3 <simdrew07@gmail.com>
2021-02-06 21:46:20 -0600

    createPlayers method created

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,20 @@
-class Game {}
+class Game {
+  constructor() {
+    this.board = new Board();
+    this.players = createPlayers();
+    this.ready = false;
+  }
+
+  /** 
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+
+  createPlayers() {
+    const players = [new Player('Player 1', '#e15258', 1, active = true),
+                     new Player('Player 2', '#e59a13', 2)];
+
+    return players;
+
+  }
+}
